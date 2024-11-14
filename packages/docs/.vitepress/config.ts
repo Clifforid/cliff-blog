bceb39452b97ea5980d83d07f032587effbd7363
Clifforid <cliffzhang422@gmail.com>
2024-11-14 17:52:11 +0800

    🎉 init: cliff blog

---
+++ packages/docs/.vitepress/config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Cliff-Blog",
+  description: "Cliff 个人站",
+  base: "/blog/",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
+    ],
+    sidebar: [
+      {
+        text: "Examples",
+        items: [
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
